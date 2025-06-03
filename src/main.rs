67c44a29c214371f6acd2f0280aecd743e86e806
Krystof Novotny <felixmorningstar@proton.me>
2025-06-03 14:39:01 +0200

    Stared with Winit to open window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Event loop and window
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("TERA_Ventl Engine")
+        .build(&event_loop)
+        .unwrap();
+
+    // Start main loop
+    event_loop.run(move |event, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent { 
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            Event::RedrawRequested(_) => {
+                // Renderer stuff - nothing for now
+            }
+            _ => (),
+        }
+    });
 }
+
