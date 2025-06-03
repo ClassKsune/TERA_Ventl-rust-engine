2c8869fbbd6ed5c6baf8766d3f698bd580e4ba72
TheUniversality <zahra.matej@gmail.com>
2025-06-03 14:56:46 +0200

    Changes to be committed: 	new file:   TS_BuildProg.sh

---
+++ TS_BuildProg.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/sh
+
+echo -n "Clean debug build? [y]"
+read V_Str_Resp
+
+if [ "$V_Str_Resp" = "y" ]; then
+	cargo clean
+	cargo build
+fi
+
+cp -r ./FLD_Resources ./target/debug/
+cp -r ./FLD_GuideFiles/ ./target/debug/
+
+cargo build
+clear
